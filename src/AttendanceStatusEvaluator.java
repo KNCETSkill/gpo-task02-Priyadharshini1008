a1570b6d456411f1f39d48707361a601f04d4c63
Priyadharshini1008 <priyadharshinip1008@gmail.com>
2026-01-19 15:40:41 +0530

    Refactor AttendanceStatusEvaluator for attendance evaluation

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -1,6 +1,16 @@
 import java.io.*;
 import java.util.*;
-public class AttendanceStatusEvaluator{
-  public static void main(String[] args){
-    
+public class AttendanceStatusEvaluator {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int attendance = sc.nextInt();
+      if ( attendance > 85)
+            System.out.println("Excellent");
+        else if (attendance  >= 60)
+            System.out.println("Satisfactory");
+        else
+            System.out.println("Poor");
+    }
+}
+
     
